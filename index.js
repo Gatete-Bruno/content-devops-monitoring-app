20b6d22ad39e27bd23784d83f88a836293a80cf4
Gatete-Bruno <catobrunoisrael@gmail.com>
2021-05-29 12:15:25 +0200

    App Monitoring Index JS

--- index.js
+++ index.js
@@ -2,6 +2,44 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var responseTime = require('response-time');
+
+const prom = require('prom-client');
+const collectDefaultMetrics = prom.collectDefaultMetrics;
+
+//Metric Definitions
+
+
+const responsetimesumm = new prom.Summary ({
+  name: 'forethought_response_time_summary',
+  help: 'Latency in percentiles',
+});
+
+
+const taskgauge = new prom.Gauge({
+  name: 'forethought_current_tasks',
+  help: 'Amount of incomplete tasks'
+});
+
+
+const tasksdone = new prom.Counter({
+  name: 'forethought_tasks_complete',
+  help: 'The number of items completed'
+});
+
+const tasksadded = new prom.Counter({
+  name: 'forethought_tasks_added',
+  help: 'The number of items added to the to-do list, total'
+});
+
+const responsetimehist = new prom.Histogram ({
+  name: 'forethought_response_time_histogram',
+  help: 'Latency in history form',
+  buckets: [0.1, 0.25, 0.5, 1, 2.5, 5, 10]
+});
+
+
+collectDefaultMetrics({ prefix: 'forethought_' });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -18,6 +56,8 @@ app.post("/addtask", function(req, res) {
   var newTask = req.body.newtask;
   task.push(newTask);
   res.redirect("/");
+  tasksadded.inc();
+  taskgauge.inc();
 });
 
 // remove a task
@@ -31,15 +71,29 @@ app.post("/removetask", function(req, res) {
     for (var i = 0; i < completeTask.length; i++) {
       complete.push(completeTask[i]);
       task.splice(task.indexOf(completeTask[i]), 1);
+      tasksdone.inc();
+      taskgauge.dec();
     }
   }
   res.redirect("/");
 });
 
+// tracking response time
+
+app.use(responseTime(function (req, res, time) {
+  responsetimesumm.observe(time);
+  responsetimehist.observe(time);
+}));
+
 // get website files
 app.get("/", function (req, res) {
   res.render("index", { task: task, complete: complete });
 });
+app.get('/metrics', function (req, res) {
+  res.set('Content-Type', prom.register.contentType);
+  res.end(prom.register.metrics());
+});
+
 
 // listen for connections
 app.listen(8080, function() {
